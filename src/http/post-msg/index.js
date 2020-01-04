12200a4e96b09b7b245541b047ca5f02ed2fe142
Ben Jeavons <bjeavons@users.noreply.github.com>
2020-01-03 22:02:03 -0800

    log body?

--- src/http/post-msg/index.js
+++ src/http/post-msg/index.js
@@ -3,9 +3,9 @@ let data = require('@begin/data');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 exports.handler = async function http(req) {
-  let msg = arc.http.helpers.bodyParser(req);
+  let msg = arc.http.helpers.bodyParser(req.body);
   const twiml = new MessagingResponse();
-  console.log('req:', req);
+  console.log('got msg:', msg);
   twiml.message('hello robo');
   return {
     statusCode: 200,
