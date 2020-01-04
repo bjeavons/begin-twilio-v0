4e658c6ca6a8b149d629ad61fc3729b56274ec54
Ben Jeavons <bjeavons@users.noreply.github.com>
2020-01-03 21:32:23 -0800

    right version of twilio

--- src/http/post-msg/index.js
+++ src/http/post-msg/index.js
@@ -1,11 +1,11 @@
 let arc = require('@architect/functions');
 let data = require('@begin/data');
-let twilio = require('twilio');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 exports.handler = async function http(req) {
-  //let msg = arc.http.helpers.bodyParser(req);
-  let twiml = new twilio.TwimlResponse();
-  console.log('got', req);
+  let msg = arc.http.helpers.bodyParser(req);
+  const twiml = new MessagingResponse();
+  console.log('body:', msg.toString);
   twiml.message('hello robo');
   return {
     statusCode: 200,
