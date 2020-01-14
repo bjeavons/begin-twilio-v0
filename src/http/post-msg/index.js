7087057a54cd294eee19e5e122814244772f41cb
Ben Jeavons <bjeavons@users.noreply.github.com>
2020-01-13 16:52:51 -0800

    try twilio req validation

--- src/http/post-msg/index.js
+++ src/http/post-msg/index.js
@@ -1,7 +1,21 @@
+let twilio = require('twilio');
 let arc = require('@architect/functions');
 let game = require('./game.js');
 
 exports.handler = async function http(req) {
+  const requestIsValid = twilio.validateRequest(
+    process.env.TWILIO_AUTH_TOKEN,
+    req.headers['x-twilio-signature'],
+    process.env.URL,
+    req.body
+  );
+  if ((process.env.NODE_ENV == 'production' || process.env.NODE_ENV == 'staging') && !requestIsValid) {
+    return {
+      statusCode: 401,
+      body: 'Unauthorized'
+    }
+  }
+
   console.log(Date());
   try {
     response = await game(arc.http.helpers.bodyParser(req));
